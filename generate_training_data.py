30d90b50d02ff4ea48c71775930faa08b319c82f
csw <11911915@mail.sustech.edu.cn>
2022-01-18 01:25:32 +0800

    modify generate_training_data

--- generate_training_data.py
+++ generate_training_data.py
@@ -3,11 +3,32 @@ import random
 
 from PIL import Image, ImageFont, ImageDraw
 
-FONT_SIZE_RANGE = [30, 30]
+# 训练参数1
+# FONT_SIZE_RANGE = [30, 30] #
+# IMAGE_WIDTH_RANGE = [28, 28]
+# IMAGE_HEIGHT_RANGE = [28, 28]
+# LETTER_LOCATION_RANGE_HOR = [-2, 16]
+# LETTER_LOCATION_RANGE_VER = [-7, 2]
+
+# FONT_SIZE_RANGE = [30, 30] #
+# IMAGE_WIDTH_RANGE = [28, 28]
+# IMAGE_HEIGHT_RANGE = [28, 28]
+# LETTER_LOCATION_RANGE_HOR = [-2, 16]
+# LETTER_LOCATION_RANGE_VER = [-7, 2]
+
+# 这组参数效果好 基本上填满了整个框
+# FONT_SIZE_RANGE = [32, 32] #
+# IMAGE_WIDTH_RANGE = [28, 28]
+# IMAGE_HEIGHT_RANGE = [28, 28]
+# LETTER_LOCATION_RANGE_HOR = [4, 4]
+# LETTER_LOCATION_RANGE_VER = [-7, -7]
+
+# 上面的参数加入随机性 训练参数2
+FONT_SIZE_RANGE = [32, 32] #
 IMAGE_WIDTH_RANGE = [28, 28]
 IMAGE_HEIGHT_RANGE = [28, 28]
-LETTER_LOCATION_RANGE_HOR = [-2, 16]
-LETTER_LOCATION_RANGE_VER = [-7, 2]
+LETTER_LOCATION_RANGE_HOR = [2, 6]
+LETTER_LOCATION_RANGE_VER = [-9, -5]
 
 
 def generate_character_image(letter: str, font_size: int, image_size: tuple, letter_location: tuple):
@@ -29,19 +50,19 @@ def generate_random_image(epoch: int):
                        random.randint(LETTER_LOCATION_RANGE_VER[0], LETTER_LOCATION_RANGE_VER[1]))
     for i in range(65, 91):
         image = generate_character_image(chr(i), font_size, image_size, letter_location)
-        path = os.path.join(current_path, 'datasets/Test_png', chr(i), 'u' + str(epoch) + '.png')
+        path = os.path.join(current_path, 'datasets/two_104000/Test_png', chr(i), 'u' + str(epoch) + '.png')
         image.save(path)
 
     for i in range(97, 123):
         image = generate_character_image(chr(i), font_size, image_size, letter_location)
-        path = os.path.join(current_path, 'datasets/Test_png', chr(i-32), 'l' + str(epoch) + '.png')
+        path = os.path.join(current_path, 'datasets/two_104000/Test_png', chr(i-32), 'l' + str(epoch) + '.png')
         image.save(path)
 
 
 def generate_folders():
     current_path = os.getcwd()
     for i in range(65, 91):
-        os.mkdir(current_path + '/datasets/Test_png/' + chr(i))
+        os.mkdir(current_path + '/datasets/two_104000/Test_png/' + chr(i))
 
     # for i in range(65, 91):
     #     os.mkdir(current_path + '\\data\\UpperCase\\' + chr(i))
