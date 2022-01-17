adc5f01eddcf32a85836b2b988933bbc814952bd
csw <11911915@mail.sustech.edu.cn>
2022-01-18 00:09:36 +0800

    modify the generate_training_data

--- generate_training_data.py
+++ generate_training_data.py
@@ -3,10 +3,11 @@ import random
 
 from PIL import Image, ImageFont, ImageDraw
 
-FONT_SIZE_RANGE = [10, 30]
-IMAGE_WIDTH_RANGE = [30, 50]
-IMAGE_HEIGHT_RANGE = [30, 50]
-LETTER_LOCATION_RANGE = [-5, 20]
+FONT_SIZE_RANGE = [30, 30]
+IMAGE_WIDTH_RANGE = [28, 28]
+IMAGE_HEIGHT_RANGE = [28, 28]
+LETTER_LOCATION_RANGE_HOR = [-2, 16]
+LETTER_LOCATION_RANGE_VER = [-7, 2]
 
 
 def generate_character_image(letter: str, font_size: int, image_size: tuple, letter_location: tuple):
@@ -24,8 +25,8 @@ def generate_random_image(epoch: int):
     font_size = random.randint(FONT_SIZE_RANGE[0], FONT_SIZE_RANGE[1])
     image_size = (random.randint(IMAGE_WIDTH_RANGE[0], IMAGE_WIDTH_RANGE[1]),
                   random.randint(IMAGE_HEIGHT_RANGE[0], IMAGE_HEIGHT_RANGE[1]))
-    letter_location = (random.randint(LETTER_LOCATION_RANGE[0], LETTER_LOCATION_RANGE[1]),
-                       random.randint(LETTER_LOCATION_RANGE[0], LETTER_LOCATION_RANGE[1]))
+    letter_location = (random.randint(LETTER_LOCATION_RANGE_HOR[0], LETTER_LOCATION_RANGE_HOR[1]),
+                       random.randint(LETTER_LOCATION_RANGE_VER[0], LETTER_LOCATION_RANGE_VER[1]))
     for i in range(65, 91):
         image = generate_character_image(chr(i), font_size, image_size, letter_location)
         path = os.path.join(current_path, 'data', chr(i), 'u' + str(epoch) + '.png')
@@ -49,5 +50,5 @@ def generate_folders():
 
 
 generate_folders()
-for i in range(0, 50):
+for i in range(0, 1000):
     generate_random_image(i)
